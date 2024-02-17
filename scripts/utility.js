589d2c632264caf392386e8e960867b0d6c066ea
HIMU005 <hashanuzzaman99@gamil.com>
2024-02-17 22:43:34 +0600

    css done now time for js

---
+++ scripts/utility.js
@@ -0,0 +1,8 @@
+function removeBgById(elementID) {
+    const element = document.getElementById(elementID);
+    element.classList.remove('bg-btn-color')
+}
+function setBgByID(element) {
+    const element = document.getElementById(elementID);
+    element.classList.add('selected-btn');
+}
