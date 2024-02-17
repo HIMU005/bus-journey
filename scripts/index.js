62bbef8299bf653371d73e204a7ae4ed34a9c307
HIMU005 <hashanuzzaman99@gamil.com>
2024-02-18 01:02:13 +0600

    add some js

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,42 @@
-console.log('index');
+let availableSeat = 40;
+let selectTotalSeat = 0;
+let fare = 0;
+const seats = document.querySelectorAll(".seat");
+for(let i = 0;i < seats.length; i++) {
+    const seat = seats[i];
+    seat.addEventListener('click', function(e) {
+        if(selectTotalSeat >= 4) {
+            alert("You can't buy more more than 4");
+            removeEventListener;
+        }
+        const nowAvailableSeat = getNumberById('available-seat');
+        setNumberById('available-seat', nowAvailableSeat-1);
+        setNumberById('selected-seat-number', ++selectTotalSeat);
+        const updatedTable= document.getElementById("selected-table");
+        const div = document.createElement("div");
+        const h1 = document.createElement("h1");
+        const h2 = document.createElement("h2");
+        const h3 = document.createElement("h3");
+        h1.innerText = seat.innerText;
+        h2.innerText = 'Economy';
+        h3.innerText = '550';
+        div.appendChild(h1);
+        div.appendChild(h2);
+        div.appendChild(h3);
+        makeItFlexByContainer(div);
+        updatedTable.appendChild(div);
+        setBackgroundByElement(seat);
+        fare+= 550;
+        setNumberById('total-price', fare);
+        if(fare>2000) {
+            const apply = document.querySelector('#apply-btn');
+            apply.classList.remove('cursor-not-allowed');
+        }
+        const phone = document.getElementById('input-phone').value;
+        if(phone.length  && selectTotalSeat) {
+            const apply = document.querySelector('#modal-open');
+            apply.classList.remove('cursor-not-allowed');
+        }
+        
+    })
+}
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,8 +1,26 @@
-function removeBgById(elementID) {
-    const element = document.getElementById(elementID);
-    element.classList.remove('bg-btn-color')
+function removeBackgroundByElement(element) {
+    // const element = document.getElementById(element);
+    element.classList.remove("bg-[#1DD100]");
 }
-function setBgByID(element) {
-    const element = document.getElementById(elementID);
-    element.classList.add('selected-btn');
+function setBackgroundByElement(element) {
+    // const element = document.getElementsByName(elementID);
+    element.classList.add("bg-[#1DD100]");
 }
+function getNumberById(elementID) {
+    const element  = document.getElementById(elementID);
+    const elementText = element.innerText;
+    const elementTextValue = parseInt(elementText);
+    return elementTextValue;
+}
+function setNumberById(elementID, value) {
+    const element  = document.getElementById(elementID);
+    element.innerText = value;
+}
+function makeItFlexByContainer(element) {
+    element.classList.add('flex');
+    element.classList.add('justify-between');   
+}
+// function removeAllowCursorById() {
+//     const element = document.querySelector(elementID);
+//     element.classList.remove('cursor-not-allowed');
+// }
